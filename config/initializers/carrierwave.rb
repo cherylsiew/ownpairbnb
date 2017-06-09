ffcdadfab6e80f74b45cdf0fc3f9d71338fcecdd
WEI MUN SIEW <cheryl.siew@gmail.com>
2017-06-09 12:32:26 +0800

    googlestorage

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV['AWS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_KEY'],
-    region:                'ap-southeast-1'
+    provider:              'Google',
+    google_storage_access_key_id:     ENV['google_storage_access_key'],
+    google_storage_secret_access_key: ENV['google_storage_secret']
   }
   
   config.storage = :fog
-  config.fog_directory  = ENV['AWS_BUCKET_NAME']
+  config.fog_directory  = ENV['google_bucket_name']
 end
 
